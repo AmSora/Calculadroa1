eebf29e6bef67e086190810d00f09a4fe4d3d42b
AmSora <yeahtwogaming@gmail.com>
2024-12-11 17:37:50 +0100

    Changes

---
+++ Calculadora1/Calculadora1/CalculadoraFunciones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculadora1
+{
+    public class CalculadoraFunciones
+    {
+        private int numero1;
+        private int numero2;
+
+        public CalculadoraFunciones(int num1, int num2)
+        {
+            numero1 = num1;
+            numero2 = num2;
+        }
+
+        public int Sumar()
+        {
+            return numero1 + numero2;
+        }
+
+        public int Restar()
+        {
+            return numero1 - numero2;
+        }
+
+        public int Multiplicar()
+        {
+            return numero1 * numero2;
+        }
+
+        public int Dividir()
+        {
+            if (numero2 == 0)
+                throw new DivideByZeroException("No se puede dividir entre cero.");
+            return numero1 / numero2;
+        }
+    }
+}
---
+++ Calculadora1/Calculadora1Tests/CalculadoraFuncionesTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculadora1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora1.Tests
+{
+    [TestClass()]
+    public class CalculadoraFuncionesTests
+    {
+        [TestMethod()]
+
+        public void SumarTest()
+        {
+            // Arrange
+            int num1 = 3;
+            int num2 = 3;
+            CalculadoraFunciones calculadora = new CalculadoraFunciones(num1, num2);
+
+            // Act
+            int resultado = calculadora.Sumar();
+
+            // Assert
+            Assert.AreEqual(6, resultado);
+        }
+
+        [TestMethod()]
+        public void RestarTest()
+        {
+            // Arrange
+            int num1 = 2;
+            int num2 = 1;
+            CalculadoraFunciones calculadora = new CalculadoraFunciones(num1, num2);
+
+            // Act
+            int resultado = calculadora.Restar();
+
+            // Assert
+            Assert.AreEqual(1, resultado);
+        }
+
+        [TestMethod()]
+        public void MultiplicarTest()
+        {
+            // Arrange
+            int num1 = 4;
+            int num2 = 1;
+            CalculadoraFunciones calculadora = new CalculadoraFunciones(num1, num2);
+
+            // Act
+            int resultado = calculadora.Multiplicar();
+
+            // Assert
+            Assert.AreEqual(3, resultado);
+        }
+
+        [TestMethod()]
+        public void DividirTest()
+        {
+            // Caso 1: División válida
+            int num1 = 6;
+            int num2 = 3;
+            CalculadoraFunciones calculadora = new CalculadoraFunciones(num1, num2);
+            int resultado = calculadora.Dividir();
+            Assert.AreEqual(2, resultado);
+
+            // Caso 2: División por cero
+            num1 = 3;
+            num2 = 0;
+            CalculadoraFunciones calculadora2 = new CalculadoraFunciones(num1, num2);
+            Assert.ThrowsException<DivideByZeroException>(() => calculadora2.Dividir());
+        }
+    }
+}
